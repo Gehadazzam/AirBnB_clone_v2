4ef32953b5b075fad570c037f78afc6f85566276
gehad <gehadgamalazzam@gmail.com>
2024-04-04 01:09:37 +0200

    set my server for deploying

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
 #Create a fake HTML file /data/web_static/releases/test/index.html
-echo "Hello kitty! How are you.xoxox" | sudo tee /data/web_static/releases/test/index.html
+echo "Hello kitty! How are you.xoxox" > /data/web_static/releases/test/index.html
 
 #Create a symbolic link /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -25,12 +25,8 @@ my_ali="location /hbnb_static/ {\
         alias /data/web_static/current/;\
     }\
 "
-sudo sed - i '/listen 80 default_server/a '"$my_ali" "$/etc/nginx/sites-available/default"
+sudo sed -i '/listen 80 default_server/a '"$my_ali" "$/etc/nginx/sites-available/default"
 
 #Don’t forget to restart Nginx after updating the configuration: 
-if pgrep "nginx" > /dev/null; then
-    sudo service nginx restart
-else
-    sudo service nginx start
-fi
+sudo service nginx restart
 
