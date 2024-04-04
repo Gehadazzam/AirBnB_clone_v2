72adf19a5d88838e4d68cadcd69bdc0612fdfb10
gehad <gehadgamalazzam@gmail.com>
2024-04-04 02:08:46 +0200

    puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,2 +1,51 @@
 # Redo the task #0 but by using Puppet
 
+# Setup web servers for deployment of web_static
+
+# Install nginx if not already installed
+package { 'nginx':
+  ensure => installed,
+}
+
+# Create directories if they don't exist
+file { '/data/web_static/shared':
+  ensure => directory,
+}
+
+file { '/data/web_static/releases/test':
+  ensure => directory,
+}
+
+# Create a fake HTML file
+file { '/data/web_static/releases/test/index.html':
+  ensure  => present,
+  content => "Hello kitty! How are you.xoxox\n",
+}
+
+# Create symbolic link
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test/',
+}
+
+# Give ownership of /data/ folder to the ubuntu user and group
+file { '/data':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
+}
+
+# Update nginx configuration to serve content
+file_line { 'nginx_hbnb_static':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => '    location /hbnb_static/ { alias /data/web_static/current/; }',
+}
+
+# Restart nginx
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File_line['nginx_hbnb_static'],
+}
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+    Fabric script that generates a .tgz archive from
+    the contents of the web_static folder of your AirBnB Clone repo,
+    using the function do_pack
+"""
+from datetime import datetime as dt
+from fabric.api import *
+
+
+def do_pack(archive_path):
+    """
+    function to return the path of archive
+    """
+
+    stamp = dt.now().strftime("%Y%m%d%H%M%S")
+    archive = "web_static_" + stamp + ".tgz"
+    local("mkdir -p versions")
+    exist = local(f"tar -czvf versions/{archive} web_static/")
+    if exist is None:
+        return None
+    else:
+        return "versions/{}".format(archive)
