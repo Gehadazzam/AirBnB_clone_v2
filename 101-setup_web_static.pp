e4faf686c161f8ab3a454daf1edc8a1a360673e4
gehad <gehadgamalazzam@gmail.com>
2024-04-04 02:31:11 +0200

    update puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,51 +1,56 @@
-# Redo the task #0 but by using Puppet
-
-# Setup web servers for deployment of web_static
-
-# Install nginx if not already installed
+#  we’d like you to install and configure an Nginx server using Puppet instead of Bash
+#update before install nijinx
+exec { 'install_system':
+    command => '/usr/bin/apt-get update',
+}
+#install nignix
 package { 'nginx':
-  ensure => installed,
+    ensure  => 'installed',
+    require => Exec['install_system']
+}
+#print hello world
+file {'/var/www/html/index.html':
+    content => 'Hello World!'
+}
+#handle redirect
+exec {'redirect':
+    command  => 'sed -i "24i\	rewrite ^/redirect https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+    provider => 'shell'
 }
 
-# Create directories if they don't exist
-file { '/data/web_static/shared':
-  ensure => directory,
+service {'nginx':
+    ensure  => running,
+    require => Package['nginx']
 }
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test':
-  ensure => directory,
-}
+  ensure => 'directory'
+} ->
 
-# Create a fake HTML file
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
 file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => "Hello kitty! How are you.xoxox\n",
-}
-
-# Create symbolic link
+  ensure  => 'present',
+  content => "Hello kitty! How are you.xoxoxo\n"
+} ->
 file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test/',
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# Give ownership of /data/ folder to the ubuntu user and group
-file { '/data':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true,
-}
-
-# Update nginx configuration to serve content
-file_line { 'nginx_hbnb_static':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => '    location /hbnb_static/ { alias /data/web_static/current/; }',
-}
-
-# Restart nginx
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File_line['nginx_hbnb_static'],
-}
