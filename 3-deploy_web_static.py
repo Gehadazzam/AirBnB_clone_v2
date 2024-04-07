ead58944d13d09477e7aff402cf39d1ac96eb4ed
gehad <gehadgamalazzam@gmail.com>
2024-04-08 01:46:07 +0200

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,1 +1,55 @@
 #!/usr/bin/python3
+"""
+    Fabric script that generates a .tgz archive from
+    the contents of the web_static folder of your AirBnB Clone repo,
+    using the function do_pack
+"""
+from datetime import datetime as dt
+from fabric.api import *
+from os.path import basename, exists
+
+env.hosts = ["54.90.13.191", "52.3.251.54"]
+
+
+def do_deploy(archive_path):
+    """
+    function to return the path of archive
+    """
+
+    if not exists(archive_path):
+        return False
+    try:
+        arc_name = basename(archive_path)
+        arc_dir = "/data/web_static/releases/{}".format(arc_name.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(arc_dir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(arc_name, arc_dir))
+        run("sudo rm -rf {}/web_static".format(arc_dir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(arc_dir))
+        return True
+    except Exception:
+        return False
+
+
+def do_pack():
+    """
+    function to return the path of archive
+    """
+
+    stamp = dt.now().strftime("%Y%m%d%H%M%S")
+    archive = "web_static_" + stamp + ".tgz"
+    local("mkdir -p versions")
+    exist = local(f"tar -czvf versions/{archive} web_static/")
+    if exist is None:
+        return None
+    else:
+        return "versions/{}".format(archive)
+
+
+def deploy():
+    """complete process"""
+    path = do_pack()
+    if path:
+        return do_deploy(path)
+    return False
