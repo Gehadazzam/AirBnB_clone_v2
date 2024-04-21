050a077668d0a30cab37e9ab6cd9ae1d725f9b64
gehad <gehadgamalazzam@gmail.com>
2024-04-21 17:31:43 +0200

    Task 2 Croute

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,5 +15,18 @@ def hello():
     return f"Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """print HBNB"""
+
+    return f"HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text_c(text):
+    """display “C ”  of the text(replace  _ symbols with a space"""
+    return f"C {text.replace('_', ' ')}"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
