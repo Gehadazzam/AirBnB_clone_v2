8c5249c0822335e3118afc04ba33c36ee85a6075
gehad <gehadgamalazzam@gmail.com>
2024-04-21 18:09:09 +0200

    Task 3-python_route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Start Flask webapplication"""
+
+
+from flask import Flask as F
+
+
+app = F(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """print the massage"""
+
+    return f"Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """print HBNB"""
+
+    return f"HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text_c(text):
+    """display “C ”  of the text(replace  _ symbols with a space"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def pythontext(text="is cool"):
+    """The default value of text is “is cool"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
