cc1440ff99508eb64a46745e30ea58df04d00658
gehad <gehadgamalazzam@gmail.com>
2024-04-21 19:10:54 +0200

    Task 4-number_route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Start Flask webapplication"""
+
+
+from flask import Flask as F
+
+
+app = F(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """print the massage"""
+
+    return f"Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """print HBNB"""
+
+    return f"HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text_c(text):
+    """display “C ”  of the text(replace  _ symbols with a space"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def pythontext(text="is cool"):
+    """The default value of text is “is cool"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ display “n is a number” only if n is an integer"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
